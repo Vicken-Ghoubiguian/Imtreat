9bf6629297f8de6b1cf37c2b1d99288428dd0fdf
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-05-21 21:54:11 +0200

    Adding important things

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+	readme_description = fh.read()
+
 setup(name='imtreat',
 version='0.1',
 description='Python package to make images treatment easily to deploy on the PyPi platform',
+long_description=readme_description,
+long_description_content_type="text/markdown",
 url='#',
 author='Wicken',
 author_email='eric.ghoubiguian@imerir.com',
 license='MIT',
 packages=['imtreat'],
 install_requires=['opencv-contrib-python'],
-zip_safe=False)
+zip_safe=False,
+python_requires='>=3.8.2')
