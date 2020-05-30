0cb723bb2d0a826e6c91c603a8b747f25d97a9c2
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-05-30 08:24:54 +0200

    Correction of error

--- tests/image_displayer_7.py
+++ tests/image_displayer_7.py
@@ -2,7 +2,7 @@ import imtreat
 
 img = imtreat.openImageFunction("images/soleil.png", 1)
 
-img = imtreat.detailEnhanceFunction(img, False)
+img = imtreat.detailEnhanceFunction(img)
 
 imtreat.displayImageFunction("experience de MMMMMOOOOIIIIII", img)
 
