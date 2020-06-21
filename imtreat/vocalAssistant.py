a83dc80458aa705cf4b8d30d85b8bddf03e6ff24
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-06-21 11:43:05 +0200

    Define the python module for the vocal assistant

---
+++ imtreat/vocalAssistant.py
@@ -0,0 +1,47 @@
+################################################
+# The vocalAssistant python module defines the
+# vocal assistant for the imtreat python package
+################################################
+
+import pyttsx3
+
+#
+class vocalAssistantClass:
+
+	# Initialize a vocalAssistantClass instance
+	def __init__(self):
+
+		self.__engine = pyttsx3.init()
+
+	# Function to say the text contained in the 'textToSay' variable
+	def sayText(self, textToSay):
+
+		self.__engine.say(textToSay)
+
+		self.__engine.runAndWait()
+
+	# Function to return the current value of volume
+	def getVolume(self):
+
+		return self.__engine.getProperty('volume')
+
+	# Function to set a new value for volume (value between 0 and 1)
+	def setVolume(self, volumeValue):
+
+		if volumeValue >= 0 and volumeValue <= 1.0:
+
+			self.__engine.setProperty('volume', volumeValue)
+
+		else:
+
+			print("Error: invalid value for 'volumeValue'")
+
+	# Function to return the current value of rate
+	def getRate(self):
+
+		return self.__engine.getProperty('rate')
+
+	# Function to set a new value for rate
+	def setRate(self, rateValue):
+
+		self.__engine.setProperty('rate', rateValue)
