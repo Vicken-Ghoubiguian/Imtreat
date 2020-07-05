a62c63b4dd510cdbc8bcd273e2cd72e0e961e360
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-05 16:39:37 +0200

    Development of 2 image_displayer examples

---
+++ tests/image_displayer/image_displayer_23.py
@@ -0,0 +1,9 @@
+import imtreat
+
+img = imtreat.imageManagerClass.openImageFunction("../images/soleil.png", 1)
+
+img = imtreat.definedModes.blackAndWhiteModeFunction(img)
+
+imtreat.imageManagerClass.displayImageFunction("experience de MMMMMOOOOIIIIII", img)
+
+imtreat.imageManagerClass.finishItFunction()
