7d41b4aa8505aa3047b69029a9f2e65967ecb1b3
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-05 18:39:40 +0200

    Defining bodyPartsDetection module

---
+++ imtreat/bodyPartsDetection.py
@@ -0,0 +1,6 @@
+##########################################################
+# The bodyPartsDetection python module is the one which apply
+# wished body parts detection to images and webcam frames
+##########################################################
+
+import cv2
