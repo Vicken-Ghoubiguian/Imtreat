dfc6ce6471e3294bd4077a777bd8f6da2b79030d
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-07-08 21:39:10 +0200

    Adding a thing to resolve bug in 25 image displayer test

--- imtreat/bodyPartsDetection.py
+++ imtreat/bodyPartsDetection.py
@@ -11,15 +11,15 @@ class bodyPartsDetectionClass:
     # Function to apply face detection
     @staticmethod
     def faceDetectionFunction(wished_image):
-        
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        
+
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
         wished_image_in_gray = cv2.cvtColor(wished_image, cv2.COLOR_BGR2GRAY)
-        
+
         faces = face_cascade.detectMultiScale(wished_image_in_gray, 1.1, 4)
-        
+
         for (x, y, w, h) in faces:
-            
+
             cv2.rectangle(wished_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            
+
         return wished_image
