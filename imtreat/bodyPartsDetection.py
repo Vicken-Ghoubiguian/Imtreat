19bc07090ffb0f2737872ce99c7d27a3282e18b6
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-07-08 21:02:12 +0200

    Defining the bodyPartsDetectionClass Python class in bodyPartsDetection.py module

--- imtreat/bodyPartsDetection.py
+++ imtreat/bodyPartsDetection.py
@@ -4,3 +4,12 @@
 ##########################################################
 
 import cv2
+
+# Specified class to apply body parts detections
+class bodyPartsDetectionClass:
+
+    # Function to apply face detection
+    @staticmethod
+    def faceDetectionFunction(wished_image):
+        
+        print("Face detection soon....\n")
