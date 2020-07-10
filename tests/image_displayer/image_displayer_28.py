911a52c91cc6d8ade42a6300650dc23ba804c9c9
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-07-10 17:59:58 +0200

    Delete image_displayer_28.py

--- tests/image_displayer/image_displayer_28.py
+++
@@ -1,9 +0,0 @@
-import imtreat
-
-img = imtreat.imageManagerClass.openImageFunction("../images/lena.png", 1)
-
-img = imtreat.bodyPartsDetectionClass.noseDetectionFunction(img)
-
-imtreat.imageManagerClass.displayImageFunction("experience de MMMMMOOOOIIIIIIIIIIIII", img)
-
-imtreat.imageManagerClass.finishItFunction()
