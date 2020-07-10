ae52402a00a1357e5fba15adc9e617ff66e69c56
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-07-10 14:45:29 +0200

    Adding the betayesDetectionFunction function in the bodyPartsDetection.py python module

--- imtreat/bodyPartsDetection.py
+++ imtreat/bodyPartsDetection.py
@@ -8,6 +8,22 @@ import cv2
 # Specified class to apply body parts detections
 class bodyPartsDetectionClass:
     
+    # Function to apply beta eyes detection
+    @staticmethod
+    def betayesDetectionFunction(wished_image, wished_color = (255, 0, 0)):
+
+        eyes_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+        wished_image_in_gray = cv2.cvtColor(wished_image, cv2.COLOR_BGR2GRAY)
+
+        eyes = eyes_cascade.detectMultiScale(wished_image, 1.1, 4)
+
+        for (x,y,w,h) in eyes:
+
+            cv2.rectangle(wished_image, (x ,y),(x+w, y+h), wished_color, 2)
+
+        return wished_image
+    
     # Function to apply eyes detection
     @staticmethod
     def eyesDetectionFunction(wished_image, wished_color = (255, 0, 0)):
