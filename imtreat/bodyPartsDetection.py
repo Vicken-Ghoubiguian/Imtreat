39459509b4c18c0e92309edfe97cfe905aa68737
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2020-07-10 14:52:27 +0200

    Adding the nose detection function in the bodyPartsDetection.py python module

--- imtreat/bodyPartsDetection.py
+++ imtreat/bodyPartsDetection.py
@@ -1,12 +1,28 @@
-##########################################################
+##############################################################
 # The bodyPartsDetection python module is the one which apply
 # wished body parts detection to images and webcam frames
-##########################################################
+##############################################################
 
 import cv2
 
 # Specified class to apply body parts detections
 class bodyPartsDetectionClass:
+    
+    # Function to apply nose detection
+    @staticmethod
+    def noseDetectionFunction(wished_image, wished_color = (255, 0, 0)):
+
+        nose_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mcs_nose.xml')
+
+        wished_image_in_gray = cv2.cvtColor(wished_image, cv2.COLOR_BGR2GRAY)
+
+        nose = nose_cascade.detectMultiScale(wished_image, 1.1, 4)
+
+        for (x,y,w,h) in nose:
+
+            cv2.rectangle(wished_image, (x ,y),(x+w, y+h), wished_color, 2)
+
+        return wished_image
     
     # Function to apply beta eyes detection
     @staticmethod
