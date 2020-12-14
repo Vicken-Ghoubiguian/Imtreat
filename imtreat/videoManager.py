a5606ab1215200b7c46e60cb46aa9c7db9dd153f
Eric Ghoubiguian <ericghoubiguian@live.fr>
2020-12-14 13:41:00 +0100

    Definition of the 'videoManager' Python module

---
+++ imtreat/videoManager.py
@@ -0,0 +1,15 @@
+#####################################################
+# The videoManager python module is the one which
+# capture and manage video streat from camera
+#####################################################
+
+import cv2
+
+# Specified class to apply video capture and management
+class videoManagerClass:
+
+	# First created function
+	@staticmethod
+	def test():
+
+		return "First config..."
